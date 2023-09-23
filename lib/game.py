153e2d9184a020ef216fc5f969ab491724be911e
Tri Hoang <trihoang09@gmail.com>
2023-09-23 15:08:33 -0600

    putting in test game

--- lib/game.py
+++ lib/game.py
@@ -8,9 +8,17 @@ pygame.init()
 screen = pygame.display.set_mode((800,400))
 pygame.display.set_caption('Tri\'s Game')
 clock = pygame.time.Clock()
+test_font = pygame.font.Font('lib/font/Pixeltype.ttf', 50)
 
-test_surface = pygame.Surface((100,200))
-test_surface.fill('Red')
+sky_surface = pygame.image.load('lib/graphics/Sky.png').convert()
+ground_surface = pygame.image.load('lib/graphics/ground.png').convert()
+text_surface = test_font.render('My Game',False,'Black').convert()
+
+snail_surface = pygame.image.load('lib/graphics/snail/snail1.png').convert_alpha()
+snail_rect = snail_surface.get_rect(midbottom = (600,300))
+
+player_surface = pygame.image.load('lib/graphics/Player/player_walk_1.png').convert_alpha()
+player_rectangle = player_surface.get_rect(midbottom = (80,300))
 
 while True:
     for event in pygame.event.get():
@@ -18,7 +26,15 @@ while True:
             pygame.quit()
             exit()
     
-    screen.blit(test_surface,(400,200))
+    screen.blit(sky_surface,(0,0))
+    screen.blit(ground_surface,(0,300))
+    screen.blit(text_surface,(350,50))
+    snail_rect.left -= 3
+    if snail_rect.left < -50:
+        snail_rect.left = 850
+    screen.blit(snail_surface,snail_rect)
+    # print(player_rectangle.left)
+    screen.blit(player_surface,player_rectangle)
 
     pygame.display.update()
     clock.tick(60)
