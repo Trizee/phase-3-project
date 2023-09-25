5027bf44656f63a7c1e7653ad63d2daee3fe1585
Tri Hoang <trihoang09@gmail.com>
2023-09-25 15:15:55 -0600

    worked on database schema

--- lib/cli.py
+++ lib/cli.py
@@ -2,10 +2,9 @@ import re
 import inquirer
 from game import game
 from sqlalchemy import (create_engine, desc,
-    Index, Column, DateTime, Integer, String)
+    Index, Column, DateTime, Integer, String, ForeignKey)
 from sqlalchemy.ext.declarative import declarative_base
-from sqlalchemy.orm import sessionmaker
-from sqlalchemy.ext.hybrid import hybrid_property
+from sqlalchemy.orm import sessionmaker, relationship, backref
 
 Base = declarative_base()
 
@@ -14,24 +13,31 @@ class Players(Base):
     __tablename__ = 'players'
 
     id =Column(Integer(),primary_key=True)
-    _name = Column('name',String())
+    name = Column('name',String())
+    description = Column('description',String())
+    scores = relationship('Scores', backref='players')
 
-    def __init__(self,name):
+    def __init__(self,name,description):
         self.name = name
-
-    @hybrid_property
-    def name(self):
-        return self._name 
-    
-    @name.setter
-    def name(self,name):
-        if isinstance(name,str) and 0 < len(name):
-            self._name = name
-        else:
-            raise ValueError('Not a valid Input')
+        self.description = description
 
     def __repr__(self):
         return f'Player: {self.name}'
+    
+class Scores(Base):
+
+    __tablename__ = 'scores'
+
+    id = Column(Integer(),primary_key=True)
+    player = Column(Integer(),ForeignKey('players.id'))
+    score = Column('score', Integer())
+
+    def __init__(self,player,score):
+        self.player = player
+        self.score = score
+
+    def __repr__(self):
+        return f'{self.score}'
     
 if __name__ == '__main__':
     engine = create_engine('sqlite:///players.db')
@@ -58,6 +64,8 @@ if __name__ == '__main__':
             view_players()
         elif main_menu_answers_key == 'Update Players':
             update_players()
+        elif main_menu_answers_key == 'View High Scores':
+            highscore()
         
     def play_menu():
         play_menu = [
@@ -77,10 +85,12 @@ if __name__ == '__main__':
     def create_new_player():
         question = [
             inquirer.Text('name', message='Enter Your Name'),
+            inquirer.Text('des', message='Give a Description'),
         ]
         answers = inquirer.prompt(question)
         new_player = Players(
-            name = answers['name']
+            name = answers['name'],
+            description = answers['des'],
         )
         if new_player.name in [player[0] for player in player_names]:
             print('Sorry Username is Already in Use')
@@ -90,11 +100,32 @@ if __name__ == '__main__':
             session.commit()
             game()
             main_menu()
-        
-        
+            
+    def highscore():
+        print(r"""
+
+                                       ._ o o
+                                       \_`-)|_
+                                    ,""       \ 
+                                  ,"  ## |   ಠ ಠ. 
+                                ," ##   ,-\__    `.
+                              ,"       /     `--._;)
+                            ,"     ## /
+                          ,"   ##    /
+
+
+                    """)
+        main_menu()
 
     def view_players():
-        print(player for player in players)
+        question = [
+            inquirer.List('update',
+                        message="Select Player to View",
+                        choices=[player for player in players],
+            ),
+        ]
+        answer = inquirer.prompt(question)
+        print(answer['update'].id)
 
     def returning_player():
         question = [
@@ -117,7 +148,7 @@ if __name__ == '__main__':
         ]
         answer = inquirer.prompt(question)
         answer_key = answer['update']
-        # print(answer_key)
         main_menu()
 
     main_menu()
+
