9e4176c6f841e1c8d43cfadf3a3971c995c8eb3f
Tri Hoang <trihoang09@gmail.com>
2023-09-25 11:10:57 -0600

    added more cli functionality

--- lib/cli.py
+++ lib/cli.py
@@ -1,6 +1,8 @@
 import re 
 import inquirer
 
+all_players = []
+
 def main_menu():
     main_menu = [
     inquirer.List('option',
@@ -12,6 +14,10 @@ def main_menu():
     main_menu_answers_key = main_menu_answers['option']
     if main_menu_answers_key == 'Play':
         play_menu()
+    elif main_menu_answers_key == 'View Players':
+        view_players()
+    elif main_menu_answers_key == 'Update Players':
+        update_players()
     
 def play_menu():
     play_menu = [
@@ -23,4 +29,30 @@ def play_menu():
     play_menu_answer = inquirer.prompt(play_menu)
     play_menu_answer_key = play_menu_answer['new']
     if play_menu_answer_key == 'New':
-        main_menu()
+        create_new_player()
+
+def create_new_player():
+    question = [
+        inquirer.Text('name', message='Enter Your Name'),
+    ]
+    answers = inquirer.prompt(question)
+    print(answers['name'])
+    all_players.append(answers['name'])
+    main_menu()
+
+def view_players():
+    print(all_players)
+
+def update_players():
+    question = [
+        inquirer.List('update',
+                    message="Select a Player to Update",
+                    choices=[player for player in all_players],
+        ),
+    ]
+    answer = inquirer.prompt(question)
+    answer_key = answer['update']
+    print(answer_key)
+    main_menu()
+
+main_menu()
